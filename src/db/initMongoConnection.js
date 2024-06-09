f7d427428b3292fcb5aa7648beca77b5de0e0ef7
tapsikk <tapsgame@gmail.com>
2024-06-09 20:22:04 +0300

    123

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -8,11 +8,19 @@ export const initMongoConnection = async () => {
     const MONGODB_URL = env('MONGODB_URL');
     const MONGODB_DB = env('MONGODB_DB');
 
-    const mongoUri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority&appName=Cluster0`;
+    console.log('MONGODB_USER:', MONGODB_USER);
+    console.log('MONGODB_PASSWORD:', MONGODB_PASSWORD);
+    console.log('MONGODB_URL:', MONGODB_URL);
+    console.log('MONGODB_DB:', MONGODB_DB);
 
-    console.log('MongoDB URI:', mongoUri); 
-    
-    await mongoose.connect(mongoUri);
+    const mongoUri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
+
+    console.log('MongoDB URI:', mongoUri);
+
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('Mongo connection successfully established!');
   } catch (e) {
     console.log('Error while setting up mongo connection', e);
