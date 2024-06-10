71310b59deb3a50a4f041bce059faa6b340e7ab5
tapsikk <tapsgame@gmail.com>
2024-06-10 21:39:34 +0300

    maybe this

--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
 import { initMongoConnection } from './db/initMongoConnection.js';
-import express from 'express';
+import { setupServer } from './server.js';
 
-const startServer = async () => {
-  try {
-    await initMongoConnection();
-    setupServer();
-  } catch (error) {
-    console.error('Error starting the server:', error);
-  }
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
 };
 
-const setupServer = () => {
-  const app = express();
-  const port = process.env.PORT || 3000;
-
-  app.use(express.json());
-
-  app.get('/', (req, res) => {
-    res.send('Hello, World!');
-  });
-
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-};
-
-startServer();
+bootstrap();
