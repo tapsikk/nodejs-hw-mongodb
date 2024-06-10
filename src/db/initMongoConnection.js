d5a9384948a7d288aa434e039f6fb9c04f072067
tapsikk <tapsgame@gmail.com>
2024-06-10 20:52:29 +0300

    123

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -8,19 +8,12 @@ export const initMongoConnection = async () => {
     const MONGODB_URL = env('MONGODB_URL');
     const MONGODB_DB = env('MONGODB_DB');
 
-    console.log('MONGODB_USER:', MONGODB_USER);
-    console.log('MONGODB_PASSWORD:', MONGODB_PASSWORD);
-    console.log('MONGODB_URL:', MONGODB_URL);
-    console.log('MONGODB_DB:', MONGODB_DB);
-
     const mongoUri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
 
     console.log('MongoDB URI:', mongoUri);
 
-    await mongoose.connect(mongoUri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(mongoUri);
+    
     console.log('Mongo connection successfully established!');
   } catch (e) {
     console.log('Error while setting up mongo connection', e);
