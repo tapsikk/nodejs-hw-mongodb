a44b93480cf35aa2c10e60f93a6682758e2a9014
tapsikk <tapsgame@gmail.com>
2024-06-14 13:28:15 +0300

    update

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -4,13 +4,13 @@ export async function getContacts(req, res) {
   try {
     const contacts = await getAllContacts();
     res.status(200).json({
-      status: '200',
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
   } catch (error) {
     res.status(500).json({
-      status: 'error 500',
+      status: 500,
       message: error.message,
     });
   }
@@ -22,18 +22,18 @@ export async function getContact(req, res) {
     const contact = await getContactById(contactId);
     if (!contact) {
       return res.status(404).json({
-        status: 'error 404',
+        status: 404,
         error: error.message,
       });
     }
     res.status(200).json({
-      status: '200',
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
   } catch (error) {
     res.status(500).json({
-      status: 'error 500',
+      status: 500,
       message: error.message,
     });
   }
