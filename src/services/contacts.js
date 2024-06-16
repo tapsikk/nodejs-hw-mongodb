a4df25d86b4a57275c0e7825cef7eed08b8b16bf
tapsikk <tapsgame@gmail.com>
2024-06-16 20:43:49 +0300

    fix db

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,21 +1,21 @@
-import Contact from '../models/Contact.js';
+import { ContactsCollection } from '../db/models/contacts.js';
 
 export const getAllContacts = async () => {
-  return await Contact.find();
+  return await ContactsCollection.find();
 };
 
 export const getContactById = async (id) => {
-  return await Contact.findById(id);
+  return await ContactsCollection.findById(id);
 };
 
 export const createNewContact = async (contactData) => {
-  return await Contact.create(contactData);
+  return await ContactsCollection.create(contactData);
 };
 
 export const updateExistingContact = async (id, contactData) => {
-  return await Contact.findByIdAndUpdate(id, contactData, { new: true });
+  return await ContactsCollection.findByIdAndUpdate(id, contactData, { new: true });
 };
 
 export const removeContact = async (id) => {
-  return await Contact.findByIdAndDelete(id);
+  return await ContactsCollection.findByIdAndDelete(id);
 };
