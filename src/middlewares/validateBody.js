a57ac75ae08bf5deb8446dae2e4d521c7952ddc7
tapsikk <tapsgame@gmail.com>
2024-06-22 20:32:29 +0300

    fix

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,16 +1,20 @@
 import Joi from 'joi';
 
-const validateBody = (schema) => {
-  return (req, res, next) => {
-    const { error } = schema.validate(req.body);
-    if (error) {
-      return res.status(400).json({
-        status: 400,
-        message: error.details[0].message,
-      });
-    }
-    next();
-  };
-};
+export const contactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().min(3).max(20).required(),
+  email: Joi.string().email().optional(),
+  isFavourite: Joi.boolean().optional(),
+  contactType: Joi.string().valid('work', 'home', 'personal').optional(),
+});
 
-export default validateBody;
+export const validateBody = (schema) => (req, res, next) => {
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({
+      status: 400,
+      message: error.details[0].message,
+    });
+  }
+  next();
+};
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,28 +1,14 @@
 import express from 'express';
-import {
-  getContacts,
-  getContact,
-  createContact,
-  updateContact,
-  deleteContact,
-} from '../controllers/contacts.js';
+import { getContacts, getContact, createContact, updateContact, deleteContact } from '../controllers/contacts.js';
 import ctrlWrapper from '../middlewares/ctrlWrapper.js';
-import validateBody from '../middlewares/validateBody.js';
-import {
-  contactSchema,
-  partialContactSchema,
-} from '../validation/contactValidation.js';
+import { validateBody, contactSchema } from '../middlewares/validateBody.js';
 
 const router = express.Router();
 
-router.get('/', ctrlWrapper(getContacts));
-router.get('/:contactId', ctrlWrapper(getContact));
-router.post('/', validateBody(contactSchema), ctrlWrapper(createContact));
-router.patch(
-  '/:contactId',
-  validateBody(partialContactSchema),
-  ctrlWrapper(updateContact)
-);
-router.delete('/:contactId', ctrlWrapper(deleteContact));
+router.get('/contacts', ctrlWrapper(getContacts));
+router.get('/contacts/:contactId', ctrlWrapper(getContact));
+router.post('/contacts', validateBody(contactSchema), ctrlWrapper(createContact));
+router.patch('/contacts/:contactId', validateBody(contactSchema), ctrlWrapper(updateContact));
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContact));
 
 export default router;
