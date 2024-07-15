090fe9a518c3120b526f9bdae0edf9fd6e521ba5
tapsikk <tapsgame@gmail.com>
2024-07-15 16:32:41 +0300

    peremoga

--- src/constants/index.js
+++ src/constants/index.js
@@ -24,3 +24,4 @@ export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
